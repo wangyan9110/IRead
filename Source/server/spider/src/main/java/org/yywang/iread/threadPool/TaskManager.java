6b0f854e8135fac88cb25896deb124e8bd67d633
wangyan9110 <wangyan9110@gmail.com>
2014-04-22 23:36:10 +0800

    add the task pool

--- Source/server/spider/src/main/java/org/yywang/iread/threadPool/TaskManager.java
+++ Source/server/spider/src/main/java/org/yywang/iread/threadPool/TaskManager.java
@@ -1,10 +1,32 @@
 package org.yywang.iread.threadPool;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * the task manager
  * is a single class
+ *
  * @author wangyan
  */
 public class TaskManager {
-	
+
+    /**
+     * the task pool max size
+     */
+    private int maxSize;
+
+    /**
+     * the task pool
+     */
+    private static final List<BaseTask> taskPool=new LinkedList<BaseTask>();
+
+    /**
+     * add the task to the pool
+     * @param baseTask
+     */
+    public void add(BaseTask baseTask){
+        taskPool.add(baseTask);
+    }
+
 }
